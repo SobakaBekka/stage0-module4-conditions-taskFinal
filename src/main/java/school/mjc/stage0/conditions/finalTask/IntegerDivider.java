1e6a5094a8b37bd46032d34713b9139e59b07750
SobakaBekka <st69704@upce.cz>
2024-02-21 20:21:18 +0100

    fixed the bug with division by zero

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,11 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
-        int result = dividend / divider;
+        double dividend_1 = (double)dividend;
+        double divider_1 = (double)divider;
+        double result = dividend_1 / divider_1;
         if(divider == 0) {
             System.out.println("division by zero");
         }else
-            if (result * divider == dividend) {
+            if (result * divider_1 == dividend_1) {
                 System.out.println("can be divided completely");
             } else {
                 System.out.println("cannot be divided completely");
